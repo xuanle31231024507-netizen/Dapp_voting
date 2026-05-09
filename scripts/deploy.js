75b8d9065242d9928e7bb5bac7c589dc17189e75
ThanhXuanLe <xuanle.31231024507@st.ueh.edu.vn>
2026-05-09 08:12:07 +0700

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+async function main() {
+
+    const Voting = await ethers.getContractFactory("Voting");
+
+    const voting = await Voting.deploy();
+
+    await voting.waitForDeployment();
+
+    console.log(
+        "Contract deployed to:",
+        await voting.getAddress()
+    );
+}
+
+main().catch((error) => {
+
+    console.error(error);
+
+    process.exitCode = 1;
+});
