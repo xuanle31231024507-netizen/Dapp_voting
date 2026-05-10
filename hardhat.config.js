0e86136c13754a4ebc02aeae10e148748f381a8f
ThanhXuanLe <xuanle.31231024507@st.ueh.edu.vn>
2026-05-10 14:50:42 +0700

    Deploy voting contract to Sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.20",
+
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
